458471a25bca514c6620827018762010a984a032
Brandon Choi <bchoiprogramming@gmail.com>
2024-12-15 01:45:37 -0800

    Added note in valid-parentheses

--- LeetCode/valid-parentheses.py
+++ LeetCode/valid-parentheses.py
@@ -1,3 +1,6 @@
+'''
+- Use a stack (can be implemented using the list from python)
+'''
 class Solution:
     def isValid(self, s: str) -> bool:
         bracketStack = []
@@ -6,6 +9,7 @@ class Solution:
             if bracket in '({[':
                 bracketStack.append(bracket)
             else:
+                # -1 looks at top element (aka the end of the list)
                 peekElement = bracketStack[-1] if bracketStack else None
                 if (
                     (bracket == ')' and peekElement == '(') or \
